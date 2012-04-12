47fc61e7a51fb549e231466b858ac5f10c415363
Patrick De La Garza <patrickdlg@gmail.com>
2012-04-12 00:03:11 -0400

    I added file input stream... this one compiles

--- src/TandemTree.java
+++ src/TandemTree.java
@@ -4,13 +4,14 @@ import org.antlr.runtime.RecognitionException;
 import org.antlr.runtime.TokenStream;
 import org.antlr.runtime.tree.CommonTree;
 import java.io.*;
-import org.antlr.runtime.TokenStream;
+import org.antlr.runtime.*;
 
 
 public class TandemTree{
 	public static void main(String args[]){
 		try{
-		TanGLexer lex = new TanGLexer(new FileInputStream(args[0]));
+
+		TanGLexer lex = new TanGLexer(new ANTLRInputStream(new FileInputStream(args[0])));
 		TokenStream ts = new CommonTokenStream(lex);
 		TanGParser parse = new TanGParser(ts);
 		parse.tanG();
