32b10fd0b7d9a918070d4892d80d82b56acc2a76
Jenee Benjamin <jlb2229@columbia.edu>
2012-05-03 19:06:56 -0400

    added some comments, need to do pipe translation

--- src/TreeWalker.java
+++ src/TreeWalker.java
@@ -7,46 +7,59 @@ import java.io.*;
 import org.antlr.runtime.*;
 
 public class TreeWalker {	
-	public void walkTree(CommonTree t, String filename) {
+
+	public void walkTree(CommonTree t, String filename) 
+	{
 		try {
 		BufferedWriter out = new BufferedWriter(new FileWriter(filename + ".rb"));
 		walk((CommonTree) t, out);
-		//traverse all the nodes of the root
-		for ( int i = 0; i < t.getChildCount(); i++ ) {
+		//traverse all the child nodes of the root
+		for ( int i = 0; i < t.getChildCount(); i++ ) 
+		{
 			walk(((CommonTree)t.getChild(i)), out);
 		}
 		out.close();
 		}
 		catch (IOException e) {}
 	}
-	public void walk(CommonTree t, BufferedWriter out){
+	public void walk(CommonTree t, BufferedWriter out)
+	{
 		try{
 		if ( t != null ) {			
 			//	every unary operator needs to be preceded by a open parenthesis and ended with a closed parenthesis
-				switch(t.getType()){
+				switch(t.getType())
+				{
 					case TanGParser.ADDSUB:	
-						if(t.getChildCount() > 1){
+						//if the operation is binary, read the two children and output that to the ruby code
+						if(t.getChildCount() > 1)
+						{
 							walk((CommonTree)t.getChild(0), out);
 							out.write(t.getText() + " ");
 							walk((CommonTree)t.getChild(1), out);
 						}
+						//if the operation is a unary minus, surround the right-hand side with parentheses
+						//this is to differenciate between unary operators and operations done within assignment operator
 						else{
-							if(t.getText().equals("- ")){
+							if(t.getText().equals("- "))
+							{
 								out.write("(");
 								out.write(t.getText());
 								walk((CommonTree)t.getChild(0), out);
 								out.write(")");
 							}
-							else{
+							else
+							{
 								walk((CommonTree)t.getChild(0), out);
 							}
 						}
 						break;
+						//binary operations like this simply prints out the 1st child, the operation and the 2nd child
 					case TanGParser.AND:
 						walk((CommonTree)t.getChild(0), out);
 						out.write(t.getText() + " ");
 						walk((CommonTree)t.getChild(1), out);
 						break;
+						//expressions like these do not require translation and can simply to outputed to the ruby file
 					case TanGParser.ASSERT:
 						out.write(t.getText() + " ");
 						break;					
@@ -55,6 +68,7 @@ public class TreeWalker {
 						out.write( t.getText() + " ");
 						walk((CommonTree)t.getChild(1), out);
 						break;
+						//this operator and a few of the following operators are different in ruby so a translation was necessary
 					case TanGParser.BITAND:
 						walk((CommonTree)t.getChild(0), out);
 						out.write("& ");
@@ -95,8 +109,8 @@ public class TreeWalker {
 						out.write(t.getText() + " ");
 						break;
 					case TanGParser.BYTE:
+						out.write(t.getText() + " ");
 						break;
-
 					case TanGParser.COMMA:
 						out.write(t.getText() + " ");
 						break;
@@ -104,23 +118,31 @@ public class TreeWalker {
 						out.write(t.getText());
 						break;
 					case TanGParser.COND:
-						
-						for (int j = 1; j < t.getChildCount(); j=j+2 ) {
-							if(j ==  t.getChildCount()-3){
+						//we start at the second child node and skip every other one to skip the newlines
+						for (int j = 1; j < t.getChildCount(); j=j+2 ) 
+						{	
+							//for all the conditions, except the last, begin it with the keyword "when"
+							//begin the last condition with else
+							if(j ==  t.getChildCount()-3)
+							{
 								out.write("else ");
 								walk((CommonTree)t.getChild(j), out);
 							}
-							else if(j <  t.getChildCount()-3){
+							else if(j <  t.getChildCount()-3)
+							{
 								out.write("when ");
 								walk((CommonTree)t.getChild(j), out);
-							}else{
+							}else
+							{
 								walk((CommonTree)t.getChild(j), out);
 							}
 						}
 						break;
 					case TanGParser.CONTINUE:
+						out.write("next ");
 						break;
 					case TanGParser.DO:
+						out.write(t.getText() + " ");
 						break;
 					case TanGParser.DOT:
 						out.write(t.getText());
@@ -132,12 +154,16 @@ public class TreeWalker {
 						out.write(t.getText());
 						break;					
 					case TanGParser.EOF:
+						out.write(t.getText());
 						break;
 					case TanGParser.EQTEST:	
 						walk((CommonTree)t.getChild(0), out);
 						out.write(t.getText() + " ");
 						walk((CommonTree)t.getChild(1), out);
-						for ( int i = 2; i < t.getChildCount()-1; i++ ) {
+						//if the equality test has more than 2 child nodes, it is because it is involved in a cond
+						//in this case, just print out all the following children
+						for ( int i = 2; i < t.getChildCount()-1; i++ )
+					       	{
 							walk((CommonTree)t.getChild(i), out);
 						}
 						break;
@@ -150,6 +176,7 @@ public class TreeWalker {
 						walk((CommonTree)t.getChild(1), out);
 						break;
 					case TanGParser.EXPONENT:
+						//the power 10 operator in Tandem is simply e. It needs to be transformed to ruby code.
 						out.write("(");
 						walk((CommonTree)t.getChild(0), out);
 						out.write("* 10 ** ");
@@ -214,7 +241,10 @@ public class TreeWalker {
 						walk((CommonTree)t.getChild(0), out);
 						out.write(t.getText() + " ");
 						walk((CommonTree)t.getChild(1), out);
-						for ( int i = 2; i < t.getChildCount()-1; i++ ) {
+						//if the magnitude comparison has more than 2 child nodes, it is because it is involved in a cond
+						//in this case, just print out all the following children
+						for ( int i = 2; i < t.getChildCount()-1; i++ )
+					       	{
 							walk((CommonTree)t.getChild(i), out);
 						}
 						break;					
@@ -232,20 +262,25 @@ public class TreeWalker {
 						out.write(t.getText());
 						break;
 					case TanGParser.NODE:
-						
-						if (t.getText().equals("public node")){
+						//every node will be converted to a class with the name of the node as the class name
+						if (t.getText().equals("public node"))
+						{
 							out.write("class ");
 							walk((CommonTree)t.getChild(0), out);
 						}
-						else{
+						//if the class is private, add private after writing the constructor of the class
+						else
+						{
 							out.write("class ");
 							walk((CommonTree)t.getChild(0), out);
 							out.newLine();
 							out.write("private");
 						}
 						out.newLine();
+						//then each class will have a main method with the node definition code
 						out.write("def main");
-						for ( int i = 1; i < t.getChildCount(); i++ ) {
+						for ( int i = 1; i < t.getChildCount(); i++ ) 
+						{
 							walk((CommonTree)t.getChild(i), out);
 						}
 						out.newLine();
@@ -254,7 +289,10 @@ public class TreeWalker {
 					case TanGParser.NOT:
 						out.write(t.getText());
 						walk((CommonTree)t.getChild(0), out);
-						for ( int i = 1; i < t.getChildCount()-1; i++ ) {
+						//if the not statement has more than 1 child nods, it is because it is involved in a cond
+						//in this case, just print out all the following children
+						for ( int i = 1; i < t.getChildCount()-1; i++ ) 
+						{
 							walk((CommonTree)t.getChild(i), out);
 						}
 						break;	
@@ -307,7 +345,7 @@ public class TreeWalker {
 						out.write(t.getText() + " ");
 						break;
 					case TanGParser.UNLESS:
-						out.write("! while");
+						out.write(t.getText() + " ");
 						break;
 					case TanGParser.UNTIL:
 						out.write(t.getText() + " ");
