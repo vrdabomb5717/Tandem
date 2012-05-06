54d786393773e558ec28660689d899321db53605
donald-e-pomeroy <dep2127@gmail.com>
2012-05-06 07:22:57 -0400

    Added test for geometry question 3

---
+++ test/tutorial/geometry_question3_test.rb
@@ -0,0 +1,10 @@
+require_relative 'geometry_question1'
+require "test/unit"
+ 
+class TestGeometryQuestion3 < Test::Unit::TestCase
+ 
+  def test_simple
+    assert_equal(184, Solve_question3.new().main(92,2))    
+  end
+ 
+end
